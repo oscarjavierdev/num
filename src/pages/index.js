20c0867977460aad1c71cef447104dc750929c07
oscarjavierdev <oscarjavierdev@gmail.com>
2025-04-22 20:18:14 -0500

    base app

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Welcome to the NUM App</h1>
+    </div>
+  );
+}
